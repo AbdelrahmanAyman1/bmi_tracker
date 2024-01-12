d8c75f106798693acccd4fc313600c9d521b3e58
boody <boodayman936@gmail.com>
2024-01-12 19:27:28 +0200

    finish sing out ui

--- lib/screens/tabs/sing_out_tab.dart
+++ lib/screens/tabs/sing_out_tab.dart
@@ -1,12 +1,35 @@
+import 'package:bmi_tracker/screens/sing_in_screen.dart';
+import 'package:bmi_tracker/widgets/custom_button.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class SingOutTab extends StatelessWidget {
   const SingOutTab({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      color: Colors.grey,
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.stretch,
+      children: [
+        SizedBox(height: 50.h),
+        Image.asset('assets/images/logout.png',
+            width: double.infinity.w, height: 300.h),
+        SizedBox(height: 50.h),
+        Text(
+          textAlign: TextAlign.center,
+          'You want to sing out',
+          style: TextStyle(fontSize: 22.sp, fontWeight: FontWeight.bold),
+        ),
+        SizedBox(height: 20.h),
+        Padding(
+          padding: EdgeInsets.symmetric(horizontal: 20.w),
+          child: CustomButton(
+            onPressed: () {},
+            text: 'Sing in',
+          ),
+        )
+      ],
     );
   }
 }
