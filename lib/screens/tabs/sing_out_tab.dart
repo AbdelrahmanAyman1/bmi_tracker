18e033377ac727d87d12a6899d8765110b73972e
boody <boodayman936@gmail.com>
2024-01-12 20:52:04 +0200

    finish sing out logic

--- lib/screens/tabs/sing_out_tab.dart
+++ lib/screens/tabs/sing_out_tab.dart
@@ -1,3 +1,5 @@
+import 'package:awesome_dialog/awesome_dialog.dart';
+import 'package:bmi_tracker/helper/show_dialog.dart';
 import 'package:bmi_tracker/screens/sing_in_screen.dart';
 import 'package:bmi_tracker/widgets/custom_button.dart';
 import 'package:firebase_auth/firebase_auth.dart';
@@ -25,7 +27,15 @@ class SingOutTab extends StatelessWidget {
         Padding(
           padding: EdgeInsets.symmetric(horizontal: 20.w),
           child: CustomButton(
-            onPressed: () {},
+            onPressed: () {
+              awesomeDialog('warning', 'Are you sure you want to sing out',
+                  context, DialogType.warning,
+                  dismissOnTouchOutside: false,
+                  btnCancelOnPress: () {}, btnOkOnPress: () async {
+                await FirebaseAuth.instance.signOut();
+                Navigator.pushReplacementNamed(context, SingInScreen.routeName);
+              });
+            },
             text: 'Sing in',
           ),
         )
